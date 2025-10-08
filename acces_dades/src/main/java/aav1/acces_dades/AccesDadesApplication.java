2e8dfe8fcc138b455cc7df15b6b58a503e8e436a
Jime1805 <18ejimen@gmail.com>
2025-10-08 19:49:43 +0200

    repo creat

---
+++ acces_dades/src/main/java/aav1/acces_dades/AccesDadesApplication.java
@@ -0,0 +1,13 @@
+package aav1.acces_dades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesDadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesDadesApplication.class, args);
+	}
+
+}
---
+++ acces_dades/src/test/java/aav1/acces_dades/AccesDadesApplicationTests.java
@@ -0,0 +1,13 @@
+package aav1.acces_dades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesDadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
